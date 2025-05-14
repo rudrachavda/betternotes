4a0172093f1292d02fcfd1ab2e91f861789d5a3e
Rudra Chavda <rudrabchavda@gmail.com>
2025-05-14 13:01:05 -0700

    added sidebar and functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -17,6 +17,20 @@ export const Navigation = () => {
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+    useEffect (() => {
+        if (isMobile) {
+            collapse();
+        } else {
+            resetWidth();
+        }
+    }, [isMobile]);
+
+    // useEffect (() => {
+    //     if (isMobile) {
+    //         collapse();
+    //     }
+    // }, [pathname, isMobile]);
+
     const handleMouseDown = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ) => {
@@ -46,12 +60,42 @@ export const Navigation = () => {
         isResizingRef.current = false;
         document.removeEventListener("mousemove", handleMouseMove);
         document.removeEventListener("mouseup", handleMouseUp);
-    }
+    };
 
-    // chat added, may or may not need to be changed later on
-    useEffect(() => {
-        setIsCollapsed(isMobile)
-    }, [isMobile])
+    const resetWidth = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(false);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = isMobile ? "100" : "240px";
+            navbarRef.current?.style.setProperty(
+                "width",
+                isMobile ? "0" : "calc(100% - 240px)"
+            );
+            navbarRef.current.style.setProperty(
+                "left",
+                isMobile ? "100%" : "240px"
+            );
+            setTimeout(() => setIsResetting(false), 300);
+        }
+    };
+
+    const collapse = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(true);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = "0";
+            navbarRef.current.style.setProperty("width", "100%");
+            navbarRef.current.style.setProperty("left", "0");
+            setTimeout(() => setIsResetting(false), 300)
+        }
+    };
+
+        // chat added, may or may not need to be changed later on
+        useEffect(() => {
+            setIsCollapsed(isMobile)
+        }, [isMobile])
 
     return (
         <>
@@ -64,6 +108,7 @@ export const Navigation = () => {
                 )}
             >
                 <div
+                    onClick={collapse}
                     role="button"
                     className={cn(
                         "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -81,11 +126,11 @@ export const Navigation = () => {
                     <p>Documents</p>
                 </div>
 
-                <div 
-                onMouseDown={handleMouseDown}
-                onClick={() => {}}
-                className="opacity-0 group-hover/sidebar:opacity-100 
-            transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
+                <div
+                    onMouseDown={handleMouseDown}
+                    onClick={() => { }}
+                    className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+                    />
 
             </aside>
             <div
@@ -97,7 +142,7 @@ export const Navigation = () => {
                 )}
             >
                 <nav className="bg-transparent px-3 py-2 w-full">
-                    {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
                 </nav>
             </div>
         </>
